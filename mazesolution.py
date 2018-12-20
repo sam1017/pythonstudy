a8ef26d3766c17f4295d90b54ef62705fbbc356e
liangchangwei <liangchangwei-os@360os.com>
2018-12-20 11:22:22 +0800

    maze solution finished

--- mazesolution.py
+++ mazesolution.py
@@ -27,19 +27,40 @@ valid_seats = [ seat(1,1),seat(1,2),seat(1,4),seat(1,5),seat(1,6),seat(1,8),
                 seat(7,1),seat(7,5),seat(7,8),
                 seat(8,2),seat(8,3),seat(8,4),seat(8,5),seat(8,6),seat(8,7),seat(8,8)]
 
+def check_if_contain(seat_step, valid_seats):
+    for valid_seat in valid_seats:
+        if seat_step.get_x() == valid_seat.get_x() and seat_step.get_y() == valid_seat.get_y():
+            return True
+    return False
+
 def seat_add(start, step):
     return seat(start.get_x()+ step.get_x(), start.get_y() + step.get_y())
 
-seat_steps = [seat(0,1),seat(1,0),seat(0,-1),seat(-1,0)]
-
-#def new_step_seat(valid_seats, searched_seats, start):
+def new_step_seat(valid_seats, searched_seats, start, seat_steps):
+    for seat_step in seat_steps:
+        new_seat = seat_add(start, seat_step)
+        if check_if_contain(new_seat, valid_seats):
+            if check_if_contain(new_seat, searched_seats) == False:
+                return new_seat
+    return None
 
 def mazepath(valid_seats,start,end):
     solution_path = []
     searched_seats = []
     solution_path.append(start)
-    searched_seats.insert(start)
-    #while end not in solution_path:
+    searched_seats.insert(0, start)
+    seat_steps = [seat(0,1),seat(1,0),seat(0,-1),seat(-1,0)]
+    while check_if_contain(end, solution_path) == False and len(solution_path) > 0:
+        start_seat = solution_path[-1]
+        #start_seat.show()
+        new_start_seat = new_step_seat(valid_seats, searched_seats, start_seat, seat_steps)
+        if  new_start_seat!= None:
+            print("new_start_seat ( " + str(new_start_seat.get_x())+ "," + str(new_start_seat.get_y())+ ")")
+            solution_path.append(new_start_seat)
+            searched_seats.insert(0, new_start_seat)
+        else:
+            seat_pop = solution_path.pop()
+            print("solution_path.pop() ( " + str(seat_pop.get_x())+ "," + str(seat_pop.get_y())+ ")")
     return solution_path
 
 
