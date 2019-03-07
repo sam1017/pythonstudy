5d917cc8b9763e359ab450e463502b967033a5e2
liangchangwei <liangchangwei-os@360os.com>
2019-03-07 20:50:52 +0800

    add neural newtwork code

---
+++ simple_neural_network.py
@@ -0,0 +1,31 @@
+# -*- coding:UTF-8 -*-
+import numpy as np
+
+b = 0
+a = 0.5
+x = np.array([[0, 1, 1], [0, 1, 0], [0, 0, 0], [0, 0, 1]])
+d = np.array([1, 1, 0, 1])
+w = np.array([b, 0, 0])
+
+def sgn(v):
+    if v > 0:
+        return 1
+    else:
+        return 0
+
+def comy(myw, myx):
+    return sgn(np.dot(myw.T, myx))
+
+def neww(oldw, myd, myx, a):
+    print(oldw)
+    print(myd)
+    print(myx)
+    print(a)
+    return oldw + a * (myd - comy(oldw, myx)) * myx
+
+i = 0
+for xn in x:
+    w = neww(w, d[i], xn, a)
+    i += 1
+    print "%d or %d  => %d " % (xn[1],xn[2],comy(w,xn))
+
