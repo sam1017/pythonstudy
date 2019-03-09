27b4b8b59e84e78c72ab3d44c2325a648c43195a
liangchangwei <liangchangwei-os@360os.com>
2019-03-09 12:21:50 +0800

    add kNN code and test kode

---
+++ test_kNN.py
@@ -0,0 +1,21 @@
+# -*- coding:UTF-8 -*-
+import kNN
+#kNN.datingClassTest()
+import matplotlib
+import matplotlib.pyplot as plt
+
+kNN.datingClassTest()
+fig = plt.figure()
+ax =  fig.add_subplot(111)
+datingDataMat,datingLabels = kNN.file2matrix('datingTestSet2.txt')
+cValue = []
+for datingLabel in datingLabels:
+    if datingLabel == 1:
+        cValue.append('r')
+    elif datingLabel == 2:
+        cValue.append('g')
+    elif datingLabel == 3:
+        cValue.append('b')
+
+ax.scatter(datingDataMat[:,0], datingDataMat[:,1], c=cValue, marker='.')
+#plt.show()
